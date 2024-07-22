98bd1af9887456710ca79dd2b13e96c9a3eea246
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2024-07-22 12:17:37 +0000

    add healthcheck

--- app/cmd/server/server.go
+++ app/cmd/server/server.go
@@ -2,8 +2,12 @@ package server
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/stretchr/graceful"
+	"github.com/takahiroaoki/go-env/handler"
 )
 
 func NewServerCmd() *cobra.Command {
@@ -11,6 +15,14 @@ func NewServerCmd() *cobra.Command {
 		Use: "server",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("Hello world!")
+
+			server := http.NewServeMux()
+			server.HandleFunc("/", handler.HealthCheck)
+
+			fmt.Println("Starting web server...")
+			graceful.Run(":8080", 1*time.Second, server)
+			fmt.Println("Stopping web server...")
+
 			return nil
 		},
 	}

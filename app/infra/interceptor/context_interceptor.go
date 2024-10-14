6aa24e270d93f133d4ea36981316e4f5bf17b5c6
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2024-10-14 03:10:47 +0000

    :recycle: refactor context

--- app/infra/interceptor/context_interceptor.go
+++ app/infra/interceptor/context_interceptor.go
@@ -4,13 +4,13 @@ import (
 	"context"
 
 	"github.com/google/uuid"
-	"github.com/takahiroaoki/grpc-sample/app/constant"
+	"github.com/takahiroaoki/grpc-sample/app/domain/util"
 	"google.golang.org/grpc"
 )
 
 func SetContext() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (res any, err error) {
-		ctx = context.WithValue(ctx, constant.REQUEST_ID, uuid.New())
+		ctx = context.WithValue(ctx, util.REQUEST_ID, uuid.New())
 		res, err = handler(ctx, req)
 		return
 	}
